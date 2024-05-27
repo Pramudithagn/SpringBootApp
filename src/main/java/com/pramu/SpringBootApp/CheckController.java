dcc90d05a30204720656ceced3780a15287d914f
Pramuditha <pramudithagn@gmail.com>
2024-05-27 23:55:56 +0530

    properties change

--- src/main/java/com/pramu/SpringBootApp/CheckController.java
+++ src/main/java/com/pramu/SpringBootApp/CheckController.java
@@ -1,5 +1,6 @@
 package com.pramu.SpringBootApp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class CheckController {
 
 //    @RequestMapping (value = "/check", method = RequestMethod.GET)
+
+    @Value("${check.message}")
+    private String checkMessage;
+
     @GetMapping("/check")
     public String check(){
-        return "app checked";
+        return checkMessage;
     }
 
 }
